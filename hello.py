a2a3c8d487e6ed49b54be1219546269833eb94c6
Priyanka-Barole <priyankabarole888@gmail.com>
2023-11-29 17:47:45 +0530

    hello.py file

---
+++ hello.py
@@ -0,0 +1,34 @@
+
+
+
+# def qudretic_eq(a,b):
+# 	if a<b:
+# 		d=(a,2)**+(b,2)**+2*a*b
+#         return d
+#         else:
+#         z=(a-b)+(a+b)
+#     print (qudretic_eq(1,2))
+#     print (qudretic_eq(3,2)) 	
+
+def qudretic_eq(a,b):
+	if a<b and a<b:
+		d=(a-b)+(a+b)
+		return d
+	else:
+		z=(a-b)+(a+b)
+		return z
+	print(qudretic_eq(1,2))
+	print(qudretic_eq(3,5))   
+
+def qudretic_eq(a,b):
+    if a>b and a>b:
+    	z=a**2+b**2+2*a*b
+    	return z   
+    else:	
+    	d=a**2-b**2
+    	return d
+    print(qudretic_eq(2,6))
+    print(qudretic_eq(3,8))    	
+    			
+    			
+

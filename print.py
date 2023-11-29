3990e45f55b2af4f3655de428c4b004ee5a07e7c
Priyanka-Barole <priyankabarole888@gmail.com>
2023-11-29 18:06:12 +0530

    this my program

---
+++ print.py
@@ -0,0 +1,16 @@
+var_a = int(input("Enter integer value of a : "))
+var_b = int(input("Enter integer value of b : "))
+
+result_add = var_a+var_b
+result_sub = var_a-var_b
+result_mul = var_a*var_b
+result_div = var_a/var_b
+result_mod = var_a%var_b
+print(result_add)
+print(result_sub)
+print(result_mul)
+print(result_div)
+print(result_mod)
+
+
+
